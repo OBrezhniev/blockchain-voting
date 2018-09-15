69d30b98c9b9cb5060508067e1d1deef4dd6bd0e
Oleksandr Brezhniev <oleksandr.brezhniev@infopulse.com>
2018-09-16 02:55:00 +0300

    Routes

--- node/routes/api.js
+++ node/routes/api.js
@@ -10,7 +10,51 @@ router.post('/vote', async function (req, res) {
     let result = await votingService.vote(req.params.address, req.params.ballot);
     console.log(result);
 
-    res.send("ok");
+    res.json(result);
+
+});
+
+router.post('/addToWhiteList', async function (req, res) {
+
+    console.log("/api/addToWhiteList");
+
+    let result = await votingService.addToWhiteList(req.params.address);
+    console.log(result);
+
+    res.json(result);
+
+});
+
+router.get('/getVote', async function (req, res) {
+
+    console.log("/api/getVote");
+
+    let result = await votingService.getVote(req.query.address);
+    console.log(result);
+
+    res.json(result);
+
+});
+
+router.get('/getAddressCount', async function (req, res) {
+
+    console.log("/api/getAddressCount");
+
+    let result = await votingService.getAddressCount();
+    console.log(result);
+
+    res.json(result);
+
+});
+
+router.get('/getUniqueAddressByIndex', async function (req, res) {
+
+    console.log("/api/getUniqueAddressByIndex");
+
+    let result = await votingService.getUniqueAddressByIndex(req.query.index);
+    console.log(result);
+
+    res.json(result);
 
 });
 
